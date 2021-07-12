f8646ccfc6917b66fb998decab8cb8e3667dcf4c
yashlalpotu11 <yashrlalpotu30@gmail.com>
2021-07-12 15:48:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import logo from "./images/logo.png";
+// import SearchEngine from "./SearchEngine.js";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Yashhhh</h1>
+    </div>
+  );
+}
